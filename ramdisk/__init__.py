8a5e7abed19b0fd7c8f2ebf3440837f483e5d8e9
Poncho Dell <ponchodx@gmail.com>
2024-07-28 20:50:09 -0500

    Create  __init__.py class for library initialization.

---
+++ ramdisk/__init__.py
@@ -0,0 +1,10 @@
+# ramdisk/__init__.py
+
+# Import the RamDisk class from the core module
+from .core import RamDisk
+
+# Define the public API of the package
+__all__ = ['RamDisk']
+
+# Optionally, define the version of your library
+__version__ = '0.1.0'
